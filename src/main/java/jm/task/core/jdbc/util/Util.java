2cc156c0abf0ef2a1782280673faf4220fe44135
SuperAfanta <thefallenknight@mail.ru>
2023-05-23 21:55:35 +0300

    test

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,18 @@
 package jm.task.core.jdbc.util;
+import java.sql.*;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private final static String URL = "jdbc:mysql://localhost:3306/testbyafanta";
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "567tesla765";
+    public static Connection getConnection() {
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("OK");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
 }
